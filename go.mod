052c5c98631badcd81a7f142f0fc12b967e6ed56
Thomas Miller <tmiv74@gmail.com>
2023-12-24 21:32:46 +0000

    Initial checkin

---
+++ go.mod
@@ -0,0 +1,48 @@
+module github.com/tmiv/keyset-update
+
+go 1.20
+
+require (
+	cloud.google.com/go/secretmanager v1.11.4
+	github.com/lestrrat-go/jwx v1.2.27
+	google.golang.org/grpc v1.60.0
+)
+
+require (
+	cloud.google.com/go/compute v1.23.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v1.1.5 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/api v0.149.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20231212172506-995d672761c0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231211222908-989df2bf70f3 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231211222908-989df2bf70f3 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/uuid v1.5.0
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/cors v1.10.1
+	golang.org/x/crypto v0.16.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	sm "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	smpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/rs/cors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func update(w http.ResponseWriter, r *http.Request) {
+	c, secret_name, project_name, err := createEssentials(r.Context())
+	if err != nil {
+		log.Printf("Create Essentials %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer c.Close()
+	prevPayload, err := getSecret(r.Context(), secret_name, project_name, c)
+	if err != nil {
+		log.Printf("Prev Secret %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	prevSet, err := jwk.Parse(prevPayload)
+	if err != nil {
+		log.Printf("Parse Prev Secret %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	addNewKeyToKeyset(r.Context(), prevSet)
+	payload, err := json.Marshal(prevSet)
+	if err != nil {
+		log.Printf("Set Marshal %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = writeSecretVersion(r.Context(), secret_name, project_name, payload, c)
+	if err != nil {
+		log.Printf("Could not create secret version %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func setupcors() *cors.Cors {
+	originsenv := os.Getenv("CORS_ORIGINS")
+	if len(originsenv) > 0 {
+		origins := strings.Split(originsenv, "'")
+		options := cors.Options{
+			AllowedOrigins:   origins,
+			AllowedMethods:   []string{http.MethodPost},
+			AllowCredentials: true,
+		}
+		return cors.New(options)
+	} else {
+		return cors.Default()
+	}
+}
+
+func makeSymmRawKey(key jwk.SymmetricKey) error {
+	rawkey := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, rawkey); err != nil {
+		return err
+	}
+	err := key.FromRaw(rawkey)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func addNewKeyToKeyset(ctx context.Context, set jwk.Set) {
+	for set.Len() > 1 {
+		it := set.Iterate(ctx)
+		it.Next(ctx)
+		pair := it.Pair()
+		key := pair.Value.(jwk.Key)
+		set.Remove(key)
+	}
+	key := jwk.NewSymmetricKey()
+	key.Set("kid", uuid.NewString())
+	makeSymmRawKey(key)
+	set.Add(key)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	set := jwk.NewSet()
+	addNewKeyToKeyset(r.Context(), set)
+	addNewKeyToKeyset(r.Context(), set)
+	payload, err := json.Marshal(set)
+	if err != nil {
+		log.Fatalf("Bad news %v\n", err)
+	}
+
+	c, secret_name, project_name, err := createEssentials(r.Context())
+	if err != nil {
+		log.Printf("Create Essentials %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer c.Close()
+
+	getsecretreq := smpb.GetSecretRequest{
+		Name: "projects/" + project_name + "/secrets/" + secret_name,
+	}
+	activesecret, err := c.GetSecret(r.Context(), &getsecretreq)
+	if err != nil {
+		if status.Code(err) != codes.NotFound {
+			log.Printf("Could not get secret %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+	if activesecret == nil {
+		activesecret, err = createSecret(r.Context(), secret_name, project_name, c)
+		if activesecret == nil || err != nil {
+			log.Printf("Could not create secret %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+	err = writeSecretVersion(r.Context(), secret_name, project_name, payload, c)
+	if err != nil {
+		log.Printf("Could not create secret version %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func createEssentials(ctx context.Context) (*sm.Client, string, string, error) {
+	c, err := sm.NewClient(ctx)
+	if err != nil {
+		return nil, "", "", fmt.Errorf("Error creating secret manager client %v\n", err)
+	}
+	secret_name := os.Getenv("SECRET_NAME")
+	if len(secret_name) < 1 {
+		c.Close()
+		return nil, "", "", fmt.Errorf("no SECRET_NAME was delared")
+	}
+	project_name := os.Getenv("PROJECT_NAME")
+	if len(secret_name) < 1 {
+		c.Close()
+		return nil, "", "", fmt.Errorf("no PROJECT_NAME was delared")
+	}
+	return c, secret_name, project_name, nil
+}
+
+func writeSecretVersion(ctx context.Context, secret_name string, project_name string, payload []byte, c *sm.Client) error {
+	crc32c := crc32.MakeTable(crc32.Castagnoli)
+	checksum := int64(crc32.Checksum(payload, crc32c))
+
+	req := &smpb.AddSecretVersionRequest{
+		Parent: "projects/" + project_name + "/secrets/" + secret_name,
+		Payload: &smpb.SecretPayload{
+			Data:       payload,
+			DataCrc32C: &checksum,
+		},
+	}
+
+	_, err := c.AddSecretVersion(ctx, req)
+	return err
+}
+
+func getSecret(ctx context.Context, secret_name string, project_name string, c *sm.Client) ([]byte, error) {
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: "projects/" + project_name + "/secrets/" + secret_name + "/versions/latest",
+	}
+
+	result, err := c.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Verify the data checksum.
+	crc32c := crc32.MakeTable(crc32.Castagnoli)
+	checksum := int64(crc32.Checksum(result.Payload.Data, crc32c))
+	if checksum != *result.Payload.DataCrc32C {
+		return nil, fmt.Errorf("Data corruption detected.")
+	}
+
+	return result.Payload.Data, nil
+}
+
+func createSecret(ctx context.Context, secret_name string, project_name string, c *sm.Client) (*smpb.Secret, error) {
+	createreq := smpb.CreateSecretRequest{
+		SecretId: secret_name,
+		Parent:   "projects/" + project_name,
+		Secret: &smpb.Secret{
+			Replication: &smpb.Replication{
+				Replication: &smpb.Replication_Automatic_{
+					Automatic: &smpb.Replication_Automatic{},
+				},
+			},
+		},
+	}
+	activesecret, err := c.CreateSecret(ctx, &createreq)
+	if err != nil {
+		return nil, err
+	}
+	return activesecret, nil
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/Update", update)
+	mux.HandleFunc("/v1/Create", create)
+	corsobj := setupcors()
+	handler := corsobj.Handler(mux)
+	http.ListenAndServe("0.0.0.0:8080", handler)
+}
