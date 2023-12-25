acf32d5526c4cc877046af84213b9380a9dc2a4e
Thomas Miller <tmiv74@gmail.com>
2023-12-25 03:37:59 +0000

    add oidc verification

--- go.mod
+++ go.mod
@@ -12,14 +12,17 @@ require (
 	cloud.google.com/go/compute v1.23.3 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v1.1.5 // indirect
+	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/xenitab/go-oidc-middleware v0.0.43 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/otel v1.21.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
+	go.uber.org/ratelimit v0.2.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.13.0 // indirect
 	golang.org/x/sync v0.4.0 // indirect
@@ -36,6 +39,7 @@ require (
 require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
 	github.com/google/uuid v1.5.0
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
@@ -44,5 +48,6 @@ require (
 	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/cors v1.10.1
+	github.com/xenitab/go-oidc-middleware/oidchttp v0.0.43
 	golang.org/x/crypto v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -15,9 +15,12 @@ import (
 	sm "cloud.google.com/go/secretmanager/apiv1"
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	smpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"github.com/golang-collections/collections/set"
 	"github.com/google/uuid"
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/rs/cors"
+	"github.com/xenitab/go-oidc-middleware/oidctoken"
+	"github.com/xenitab/go-oidc-middleware/options"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -57,18 +60,17 @@ func update(w http.ResponseWriter, r *http.Request) {
 }
 
 func setupcors() *cors.Cors {
+	options := cors.Options{
+		AllowedMethods:   []string{http.MethodPost},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Authorization"},
+	}
 	originsenv := os.Getenv("CORS_ORIGINS")
 	if len(originsenv) > 0 {
 		origins := strings.Split(originsenv, "'")
-		options := cors.Options{
-			AllowedOrigins:   origins,
-			AllowedMethods:   []string{http.MethodPost},
-			AllowCredentials: true,
-		}
-		return cors.New(options)
-	} else {
-		return cors.Default()
+		options.AllowedOrigins = origins
 	}
+	return cors.New(options)
 }
 
 func makeSymmRawKey(key jwk.SymmetricKey) error {
@@ -212,10 +214,54 @@ func createSecret(ctx context.Context, secret_name string, project_name string, 
 	return activesecret, nil
 }
 
+type EmailClaims struct {
+	Email string `json:"email"`
+}
+
+func emailAllowedValidator() options.ClaimsValidationFn[EmailClaims] {
+	allow_env := strings.Split(os.Getenv("SECURITY_ALLOW"), ",")
+	allow_list := set.New()
+	for _, s := range allow_env {
+		allow_list.Insert(s)
+	}
+	return func(claims *EmailClaims) error {
+		if allow_list.Has(claims.Email) {
+			return nil
+		} else {
+			return fmt.Errorf("%s is not on the allow list", claims.Email)
+		}
+	}
+}
+
 func main() {
+	oidctok, err := oidctoken.New[EmailClaims](
+		emailAllowedValidator(),
+		options.WithIssuer(os.Getenv("SECURITY_ISSUER")),
+		options.WithRequiredTokenType("JWT"),
+		options.WithRequiredAudience(os.Getenv("SECURITY_AUDIENCE")),
+	)
+	if err != nil {
+		log.Fatalf("Error creating token parser %+v\n", err)
+	}
+	oidcmiddle := func(next func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			auth := r.Header.Get("Authorization")
+			if !strings.HasPrefix(auth, "Bearer ") {
+				fmt.Printf("No bearer %s\n", auth)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			_, err = oidctok.ParseToken(r.Context(), auth[7:])
+			if err != nil {
+				fmt.Printf("Unauthorized %v\n", err)
+				w.WriteHeader(http.StatusUnauthorized)
+			}
+			next(w, r)
+		}
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/Update", update)
-	mux.HandleFunc("/v1/Create", create)
+	mux.HandleFunc("/v1/Update", oidcmiddle(update))
+	mux.HandleFunc("/v1/Create", oidcmiddle(create))
 	corsobj := setupcors()
 	handler := corsobj.Handler(mux)
 	http.ListenAndServe("0.0.0.0:8080", handler)
